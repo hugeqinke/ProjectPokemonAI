5bbca32328dbc9d2109aaece2490eb3077b5250f
Thomas Yang <thomas.s.yang@gmail.com>
2017-06-25 02:03:34 -0400

    changed main logging to use logger, with some stdout

--- src/UrlServer.cpp
+++ src/UrlServer.cpp
@@ -38,7 +38,6 @@ namespace mainkiller {
     bool running = true;
 
     void killbill(int signo) {
-        std::cout << "bill has been slayed" << std::endl;
         running = false; 
     }
 }
@@ -46,31 +45,39 @@ namespace mainkiller {
 // TODO: move everything to a separate "Socket" class
 // Create a bunch of sockets
 int main() {
-    auto start = Clock::now(); 
+    tlog::Log log; 
+    log.activate("UrlServer"); 
 
+    log.logInfo("main - Starting the web crawler"); 
+    log.logInfo("main - Clearing file systems"); 
     // Clear directory for testing purposes
     std::system("exec rm -f ./datalogs/*");
     std::system("exec rm -f ./sockets/*"); 
-
+   
+    log.logInfo("main - Initializing user crawler process pool");  
     UrlServletPool* processPool = new UrlServletPool(1); 
+    log.logInfo("main - Initializing battle crawler process pool"); 
     BattleServletPool* battleServletPool = new BattleServletPool(1); 
+    log.logInfo("main - Initializing python crawler pool"); 
     PythonCrawlerPool* processCrawlerPool = new PythonCrawlerPool(1, battleServletPool, processPool);
  
     signal(SIGTERM, mainkiller::killbill); 
     
     pid_t mainpid = getpid(); 
     std::cout << "MAIN PID: " << mainpid << std::endl;
+    log.logInfo("main - mainpid: " + std::to_string(mainpid)); 
 
     // check for messages from the process 
     while (mainkiller::running) { 
         processPool->posixSelect();
     }
 
-    std::cout << "Killing all child processes now..." << std::endl;
+    log.logInfo("main - killing all child processes now..."); 
     
     processPool->stop(); 
     battleServletPool->stop(); 
 
+    log.logInfo("main - Done!"); 
     return 0;
  }
 
